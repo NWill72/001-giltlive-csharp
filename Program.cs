905235b87d3b2e095bd0b2c764f45b7c3e45c960
Nigel A Williams <nwilliams72@hotmail.com>
2025-05-21 13:39:41 -0400

    C# Basics

---
+++ Program.cs
@@ -0,0 +1,15 @@
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!\n\n");
+
+Console.WriteLine("GITHUB TEST CONSOLE APPLICATION BELATED BUILD DAY 2025 - BASIC C#");
+Console.WriteLine(@"===============================");
+Console.WriteLine(@"GIT COMMANDS       DESCRIPTIONS");
+Console.WriteLine(@"===============================");
+Console.WriteLine(@"INIT:              DESCRIPTION");
+Console.WriteLine(@"ADD:               DESCRIPTION");
+Console.WriteLine(@"COMMIT:            DESCRIPTION");
+Console.WriteLine(@"PUSH:              DESCRIPTION");
+Console.WriteLine(@"PULL:              DESCRIPTION");
+Console.WriteLine(@"STATUS:            DESCRIPTION");
+Console.WriteLine(@"===============================");
+Console.WriteLine("I just made some changes. WTF.");
